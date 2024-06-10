b71cbc436c98dbcd4862c21ed19e61a729aa82ff
Ratan Hussain <mdratanmia7395@gmail.com>
2024-06-10 23:52:06 +0300

    update form validation

--- src/pages/components/Form.jsx
+++ src/pages/components/Form.jsx
@@ -1,38 +1,152 @@
-import React from 'react'
-import Headers from '../../common/Headers'
-import Footer from '../../common/Footer'
+/** @format */
+
+import React, { useState } from 'react';
+import Headers from '../../common/Headers';
+import Footer from '../../common/Footer';
 
 export default function Form() {
-  return (
-      <div>
-          <Headers/>
+	let [value, setValue] = useState({
+		uname: '',
+		uemail: '',
+		uphone: '',
+		upassword: '',
+		index: '',
+	});
 
-          <div className="container">
-              <form >
-                  <h1>Sing Up Form</h1>
-                  <div className="form-input">
-                      <label htmlFor="uname">User Name :</label>
-                      <input type="text" name="uname" id="uname" />
-                  </div>
-                  <div className="form-input">
-                      <label htmlFor="uemail">Email address :</label>
-                      <input type="email" name="uemail" id="uemail" />
-                  </div>
-                  <div className="form-input">
-                      <label htmlFor="uphone">Mobile number :</label>
-                      <input type="number" name="uphone" id="uphone" />
-                  </div>
-                  <div className="form-input">
-                      <label htmlFor="upassword">Password :</label>
-                      <input type="password" name="upassword" id="upassword" />
-                  </div>
+	let changedData = (e) => {
+		let oldData = { ...value };
+		let inputName = e.target.name;
+		let inputValue = e.target.value;
+		oldData[inputName] = inputValue;
+		setValue(oldData);
+	};
 
-                  <button>Save</button>
-              </form>
-          </div>
+	let [formData, setFormData] = useState([]);
 
+	let formSubmit = (e) => {
+		e.preventDefault();
+		let newUser = {
+			uname: value.uname,
+			uemail: value.uemail,
+			uphone: value.uphone,
+			upassword: value.upassword,
+			index: value.index,
+		};
 
-          <Footer/>
-    </div>
-  )
+		let checkduplicate = formData.filter(
+			(v) => v.uemail == value.uemail || v.uphone == value.uphone
+		);
+
+		if (checkduplicate.length == 1) {
+			alert("checkduplicate");
+		} else {
+			let userData = [...formData, newUser];
+
+			setFormData(userData);
+			setValue({
+				uname: '',
+				uemail: '',
+				uphone: '',
+				upassword: '',
+				index: '',
+			});
+		}
+	};
+
+	return (
+		<div>
+			<Headers />
+
+			<div className='division'>
+				<div className='container'>
+					<form onSubmit={formSubmit}>
+						<h1>Sing Up Form</h1>
+						<div className='form-input'>
+							<label htmlFor='uname'>User Name :</label>
+							<input
+								value={value.uname}
+								type='text'
+								onChange={changedData}
+								name='uname'
+								id='uname'
+								required
+							/>
+						</div>
+						<div className='form-input'>
+							<label htmlFor='uemail'>Email address :</label>
+							<input
+								value={value.uemail}
+								type='email'
+								onChange={changedData}
+								name='uemail'
+								id='uemail'
+								required
+							/>
+						</div>
+						<div className='form-input'>
+							<label htmlFor='uphone'>Mobile number :</label>
+							<input
+								value={value.uphone}
+								type='number'
+								onChange={changedData}
+								name='uphone'
+								id='uphone'
+								required
+							/>
+						</div>
+						<div className='form-input'>
+							<label htmlFor='upassword'>Password :</label>
+							<input
+								value={value.upassword}
+								type='password'
+								onChange={changedData}
+								name='upassword'
+								id='upassword'
+								required
+							/>
+						</div>
+
+						<button>{value.index != '' ? 'Save' : 'Update'}</button>
+					</form>
+				</div>
+
+				<div className='table'>
+					<table>
+						<tr>
+							<th>S.L</th>
+							<th>User Name</th>
+							<th>Email</th>
+							<th>Phone</th>
+							<th>Password</th>
+							<th>Action</th>
+						</tr>
+
+						{formData.length >= 1 ? (
+							formData.map((v, i) => {
+								return (
+									<tr key={i}>
+										<td>{i + 1}</td>
+										<td>{v.uname}</td>
+										<td>{v.uemail}</td>
+										<td>{v.uphone}</td>
+										<td>{v.upassword}</td>
+										<td>
+											<button>Delete</button>
+											<button>Edit</button>
+										</td>
+									</tr>
+								);
+							})
+						) : (
+							<tr>
+								<td colSpan={6}>Data Not Found.</td>
+							</tr>
+						)}
+					</table>
+				</div>
+			</div>
+
+			<Footer />
+		</div>
+	);
 }
